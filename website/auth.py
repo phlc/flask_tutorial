d93c800530630bfade46d0443b2c960bc51da812
phlc <pedrocarvalho@outlook.com>
2022-09-28 19:37:03 -0300

    first commit

---
+++ website/auth.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint("auth", __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html", user="t")
+
+
+@auth.route('/logout')
+def logout():
+    return render_template("home.html", user="t")
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def signup():
+    if(request.method == "POST"):
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email)<4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name)<2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1!=password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1)<4:
+            flash('Password must be at least 4 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user="t")
